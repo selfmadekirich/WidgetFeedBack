13f6e1b33420c9762a4b0dbea964d1cc786ac923
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2021-03-01 22:49:52 +0300

    new version

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ class Quiz
        this.current = 0;
    }
    
-   // метод для получение json строки из массива ответов юзера
+   //метод для получение json строки из массива ответов юзера
      ConvertAnswersToJSON()
      {
          return JSON.stringify(this.answer);
@@ -52,14 +52,17 @@ class Answer
 }
 
 const Questions=[
-    new Question(1,1,"любите ли вы печенье?",2),
-    new Question(2,1,"любите ли вы молоко?",3),
-    new Question(3,1,"why are you gay?",4),
-    new Question(4,1,"занимались ли вы гачи-борьбой?",null),
-]
+    new Question(1,1,"Насколько сложными для вас являются задания?",2),
+    new Question(2,1,"Насколько удобен и понятен для вас интерфейс платформы?",3),
+    new Question(3,1,"Насколько хорошо работает коллаборативная деятельность?",4),
+    new Question(4,2,"Хватает ли рекомендаций по грамматике от платформы при прохождении курса?",null),
+] 
+
 
 const quiz=new Quiz(Questions);
 Init();
+const buttons=document.getElementById("buttons");
+
 
 function saveAnswer()
 {  
@@ -82,8 +85,9 @@ function Update()
     console.log(quiz.current);
     //сохраняем ответ на текущий вопрос 
     saveAnswer();
+    document.getElementById("check").innerHTML="Дальше";
 
-    quiz.current++;
+    quiz.current=quiz.questions[quiz.current].nextQuestion;
    //Проверяем, есть ли ещё вопросы
    if(quiz.current<quiz.questions.length){
        
@@ -91,62 +95,76 @@ function Update()
        document.getElementById("head").innerHTML = quiz.questions[quiz.current].text;
  
        //Удаляем старые варианты ответов
-       var temp=document.getElementById("buttons");
-       temp.innerHTML="";
+       
+       buttons.innerHTML="";
      /*
      здесь необходима функция , которая по типу вопроса создает нужные поля ответов
      тип 1 - оценка от 1 до 5 (к примеру)
-     тип 2 - развернутый ответ
+     тип 2 -  направляющий 
+     тип 3 - развернутый ответ
      */ 
-       //пока так  
-       for(let i = 0; i < 5;i++)
-       {  
-          //использую p для вывода текста ,тк у radio нет innerhtml
-          var paragraph=document.createElement("p");
-          paragraph.innerHTML=(i+1);
-          
-          //создаю radio button ,вешаю нужные аттрибуты 
-          var radiobutton=document.createElement("input");
-          radiobutton.setAttribute("type","radio");
-          radiobutton.setAttribute("name","ans_radio");
-          radiobutton.setAttribute("type","radio");
-          radiobutton.setAttribute("value",i);
-          paragraph.appendChild(radiobutton);
-          temp.appendChild(paragraph);
-       }
-     
+       //пока так
+       switch(quiz.questions[quiz.current].type)
+       {
+           case 1:CreateAnswers_firstType();break;
+           case 2:CreateAnswers_secondType();break;
+       }  
+
        document.getElementById("pages").innerHTML = (quiz.current + 1) + " / " + quiz.questions.length;  
    } else {
        alert("заглушка");
+       buttons.innerHTML="";
+       document.getElementById("head").innerHTML = "Спасибо!"
       console.log(quiz.answers);
    }
 }
  
-function Init()
-{  
-  document.getElementById("head").innerHTML = quiz.questions[quiz.current].text;
-  var temp=document.getElementById("buttons");
-   /*
-     здесь необходима функция , которая по типу вопроса создает нужные поля ответов
-     тип 1 - оценка от 1 до 5 (к примеру)
-     тип 2 - развернутый ответ
-     */ 
-       //пока так  
+/*value - значение аттрибута value radiobutton , innerhtml- текст для отображения в данном случае - номер ответа/или текст ответа */
+function CreateRadio(value,innerHtml)
+{
+     //использую p для вывода текста ,тк у radio нет innerhtml
+     var paragraph=document.createElement("p");
+     paragraph.innerHTML=innerHtml;
+     
+     //создаю radio button ,вешаю нужные аттрибуты 
+     var radiobutton=document.createElement("input");
+     radiobutton.setAttribute("type","radio");
+     radiobutton.setAttribute("name","ans_radio");
+     radiobutton.setAttribute("type","radio");
+     radiobutton.setAttribute("value",value);
+     paragraph.appendChild(radiobutton);
+     buttons.appendChild(paragraph);
+  
+}
+
+function CreateAnswers_secondType()
+{
+  CreateRadio(1,"Да");
+  CreateRadio(2,"Нет");
+  //и соответственно сделать переадресацию на нужный вопрос
+  // переадресация ( в этом контексте) - указать нужный номер след вопроса
+}
+
+function CreateAnswers_firstType()
+{
+    var descript=document.createElement("p");
+       descript.innerHTML="1-плохо,зря потратил деньги"+"\n"+"5-лучшее вложение в моей жизни";
+     buttons.appendChild(descript);
        for(let i = 0; i < 5;i++)
        {  
-          //использую p для вывода текста ,тк у radio нет innerhtml
-          var paragraph=document.createElement("p");
-          paragraph.innerHTML=(i+1);
-          
-          //создаю radio button ,вешаю нужные аттрибуты 
-          var radiobutton=document.createElement("input");
-          radiobutton.setAttribute("type","radio");
-          radiobutton.setAttribute("name","ans_radio");
-          radiobutton.setAttribute("type","radio");
-          radiobutton.setAttribute("value",i);
-          paragraph.appendChild(radiobutton);
-          temp.appendChild(paragraph);
+         CreateRadio(i,i+1);
        }
      
-       document.getElementById("pages").innerHTML = (quiz.current + 1) + " / " + quiz.questions.length;     
+}
+
+
+function Init()
+{            
+       document.getElementById("head").innerHTML="Пожалуйста, пройдите наш опрос"+
+       "\n" + "он поможет нам усовершенствовать наше приложение для вас."
+       var beginButton=document.getElementById("check");
+       beginButton.innerHTML="Начнем!";
+       quiz.current=-1;
+      
+       //document.getElementById("pages").innerHTML = (quiz.current + 1) + " / " + quiz.questions.length;     
 }
